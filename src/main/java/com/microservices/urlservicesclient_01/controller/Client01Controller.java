2538b9c10b2a1d5d98e12c90c6b3086facc41774
= <=>
2023-05-08 23:44:44 +0700

    prepare for add zipkin

--- src/main/java/com/microservices/urlservicesclient_01/controller/Client01Controller.java
+++ src/main/java/com/microservices/urlservicesclient_01/controller/Client01Controller.java
@@ -6,19 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Client01Controller {
-    //    @GetMapping("/client01")
-//    public ResponseEntity<?> getDataFromClient0() {
-//        String url = "http://localhost:8000/client00";
-//        // if u send anything with url just set to map and put to last of this response
-//        // element
-//        ResponseEntity<Client01> response = new RestTemplate().getForEntity(url, Client01.class);
-//        var client00 = response.getBody();
-//        return ResponseEntity.ok().body(Client01.builder().environment(client00.getEnvironment())
-//                .dataTransfer(client00.getDataTransfer()).build());
-//    }
+        @GetMapping("/client01")
+    public ResponseEntity<?> getDataFromClient0() {
+        String url = "http://localhost:8000/client00";
+        // if u send anything with url just set to map and put to last of this response
+        // element
+        ResponseEntity<Client01> response = new RestTemplate().getForEntity(url, Client01.class);
+        var client00 = response.getBody();
+        return ResponseEntity.ok().body(Client01.builder().environment(client00.getEnvironment())
+                .dataTransfer(client00.getDataTransfer()).build());
+    }
     @Autowired
     private Client00Proxy client00Proxy;
 
